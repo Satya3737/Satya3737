d5df1578714e4b78feb54ffd8e2dc213cafedebc
Satya3737 <141204172+Satya3737@users.noreply.github.com>
2023-08-28 11:54:18 +0530

    added for-loop-pratice

---
+++ entri-pratice/september/pratice.js
@@ -0,0 +1,11 @@
+let output = '';
+for(let i = 5; i > 0; i--){
+    output += i;
+    console.log('output loop', output);
+}
+
+let x=5;
+let y=6;
+let z=x+y;
+
+console.log('the value of z is ' + z);
